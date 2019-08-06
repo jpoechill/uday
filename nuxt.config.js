cfca4f81f72638c35c75bb770976102ccc9f13e7
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-08-05 22:22:22 -0700

    attempt update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Uday: Electronic Patient Records',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/uday-favicon.png?v=2' }
     ]
   },
 
